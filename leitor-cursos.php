c8f115ddbe2c9a1ba58f2fd5e839af7b20ffcc36
x <karolina_bp@hotmail.com>
2022-06-22 16:32:51 -0300

    file_get_contents para ler arquivo como um todo
    - Aprendemos sobre o file() que le o arquivo e transforma em um array

--- leitor-cursos.php
+++ leitor-cursos.php
@@ -4,7 +4,7 @@
     //parâmetros
     //nome arquivo, o que fazer com ele (r = read/ler)
     //Retorna como um ponteiro para esse arquivo
-$arquivo = fopen('lista-cursos.txt', 'r');
+// $arquivo = fopen('lista-cursos.txt', 'r');
 
 //nao chegar no fim do arquivo
 //feof = file - end of file = 
@@ -22,21 +22,30 @@ $arquivo = fopen('lista-cursos.txt', 'r');
 //filesize() = traz o tamanho do arquivo
     //Parâmetros
     //Nome do arquivo
-$tamanhoDoArquivo = filesize('lista-cursos.txt');
+// $tamanhoDoArquivo = filesize('lista-cursos.txt');
 
 //Posso pegar o arquivo todo ao invés de linha a linha
     //fread = lê o arquivo como um todo
         //Parâmetros
         //arquivos
         //quantidade de caracteres (na verdade é bytes) que quero ler
-$cursos = fread($arquivo, $tamanhoDoArquivo);
+// $cursos = fread($arquivo, $tamanhoDoArquivo);
 //Não interessante ler assim arquivos grandes
 //O php pode ler até 128 mega
 //Melhor ler linha a linha
 
-echo $cursos;
+// echo $cursos;
 
 //é sempre uma boa prática fechar o arquivo depois
 //o php faz isso sozinho, mas se acontecer algum problema o arquivo pode ficar aberto
 //isso impediria de ser manipulado por outro programas e ainda ocupa memória
-fclose($arquivo);
+// fclose($arquivo);
+
+//Forma mais simples de ler um arquivo
+//file_get_contents = abre o arquivo, busca todo o conteúdo desse arquivo para retornar como string e fecha arquivo
+echo file_get_contents('lista-cursos.txt') . "\n";
+
+echo "\n";
+
+//Lê um arquivo e transforma em um array
+var_dump(file('lista-cursos.txt'));
