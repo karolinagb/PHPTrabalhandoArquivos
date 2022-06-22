94ca35408c4b69b3dbcb1bdf329f09c4e03c0561
x <karolina_bp@hotmail.com>
2022-06-22 16:21:01 -0300

    Lendo o arquivo como um todo sem ser linha por linha

--- leitor-cursos.php
+++ leitor-cursos.php
@@ -8,16 +8,33 @@ $arquivo = fopen('lista-cursos.txt', 'r');
 
 //nao chegar no fim do arquivo
 //feof = file - end of file = 
-while(!feof($arquivo)){
-    //ler uma linha
-    //fgets = pega uma string até final da linha de determinado arquivo. Ela também posiciona o curso no final da linha lida
-    //quando a função lê uma quebra de linha ela para
+// while(!feof($arquivo)){
+//     //ler uma linha
+//     //fgets = pega uma string até final da linha de determinado arquivo. Ela também posiciona o curso no final da linha lida
+//     //quando a função lê uma quebra de linha ela para
+//         //Parâmetros
+//         //arquivo
+//         //Número = caso queira ler uma determinada quantidade de caracteres
+//     $curso = fgets($arquivo);
+//     echo $curso . PHP_EOL;
+// }
+
+//filesize() = traz o tamanho do arquivo
+    //Parâmetros
+    //Nome do arquivo
+$tamanhoDoArquivo = filesize('lista-cursos.txt');
+
+//Posso pegar o arquivo todo ao invés de linha a linha
+    //fread = lê o arquivo como um todo
         //Parâmetros
-        //arquivo
-        //Número = caso queira ler uma determinada quantidade de caracteres
-    $curso = fgets($arquivo);
-    echo $curso . PHP_EOL;
-}
+        //arquivos
+        //quantidade de caracteres (na verdade é bytes) que quero ler
+$cursos = fread($arquivo, $tamanhoDoArquivo);
+//Não interessante ler assim arquivos grandes
+//O php pode ler até 128 mega
+//Melhor ler linha a linha
+
+echo $cursos;
 
 //é sempre uma boa prática fechar o arquivo depois
 //o php faz isso sozinho, mas se acontecer algum problema o arquivo pode ficar aberto
