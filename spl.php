fdc499621086a38238dc3b58b67a52ff04a14a82
x <karolina_bp@hotmail.com>
2022-06-24 14:45:52 -0300

    Tratando arquivos como objetos

---
+++ spl.php
@@ -0,0 +1,22 @@
+<?php
+
+//spl = Seek to specified line
+
+//Não preciso informar o modo de leitura r pois o de leitura já é o padrão
+$arquivoCursos = new SplFileObject('cursos.csv', 'r');
+
+//Enquanto não chegar no final do arquivo eu vou ler uma linha em csv
+while(!$arquivoCursos->eof()){
+    $linha = $arquivoCursos->fgetcsv(';');
+
+    echo $linha[0] . "\t" . $linha[1] . PHP_EOL;
+}
+
+//TimeStamp de quando esse arquivo foi criado
+// echo $arquivoCursos->getCTime();
+
+$date = new DateTime();
+
+$date->setTimestamp($arquivoCursos->getCTime());
+
+echo $date->format('d/m/Y');
