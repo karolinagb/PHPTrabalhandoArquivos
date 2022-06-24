1224fd14debee131a724d4f251a40e671eb330e8
x <karolina_bp@hotmail.com>
2022-06-24 11:42:18 -0300

    Lendo um arquivo zip com senha

--- leitor-zip.php
+++ leitor-zip.php
@@ -1,4 +1,13 @@
 <?php
 
+//contextos para o wrappers do php
+
+//Adicionando contexto para ler zio com senha
+$contexto = stream_context_create(['zip' => [
+    'password' => '123456'
+]]);
+
 //zip:// é o wrapper ou protocolo que vamos utilizar para leitura desse arquivo
-echo file_get_contents('zip://arquivos.zip#lista-cursos.txt');
+echo file_get_contents('zip://arquivos.zip#lista-cursos.txt', false, $contexto);
+
+//Dá para usar contexto em qualquer uma das funções de stream como a fopen()
